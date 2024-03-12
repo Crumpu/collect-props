e2c5f4d939e54b8cb46247a87464aa1f00b73406
denis <denispolyarush1405@gmail.com>
2024-03-12 15:01:16 +0300

    Create new branch "checkEmail"

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ class Person {
 const btnOk = document.getElementById('ok');
 
 function changeInput(e) {
-    // e.preventDefault();
-     const userInfo = document.querySelectorAll(
-       '#f-name, #l-name, #d-name, #email'
-     );
-     const arrayUserInfo = Array.from(userInfo).map((el) => el.value);
-     const [fName, lName, dName, email] = arrayUserInfo;
-    const user = new Person(fName, lName, dName, email);
-    localStorage.setItem(`${lName}`, JSON.stringify(user))
+  // e.preventDefault();
+  const userInfo = document.querySelectorAll(
+    '#f-name, #l-name, #d-name, #email'
+  );
+  const arrayUserInfo = Array.from(userInfo).map((el) => el.value);
+  const [fName, lName, dName, email] = arrayUserInfo;
+  const user = new Person(fName, lName, dName, email);
+  localStorage.setItem(`${lName}`, JSON.stringify(user));
 }
 
 btnOk.addEventListener('click', changeInput);
