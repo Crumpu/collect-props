7147b84a496891bb20f04f5169ffbd6ad2adc56e
denis <denispolyarush1405@gmail.com>
2024-03-12 14:48:59 +0300

    finished

---
+++ src/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+class Person {
+  constructor(fName, lName, dName, email) {
+    this.fName = fName;
+    this.lName = lName;
+    this.dName = dName;
+    this.email = email;
+  }
+}
+
+const btnOk = document.getElementById('ok');
+
+function changeInput(e) {
+    // e.preventDefault();
+     const userInfo = document.querySelectorAll(
+       '#f-name, #l-name, #d-name, #email'
+     );
+     const arrayUserInfo = Array.from(userInfo).map((el) => el.value);
+     const [fName, lName, dName, email] = arrayUserInfo;
+    const user = new Person(fName, lName, dName, email);
+    localStorage.setItem(`${lName}`, JSON.stringify(user))
+}
+
+btnOk.addEventListener('click', changeInput);
