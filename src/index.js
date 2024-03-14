420b83784fb087005f8af63408a100e300c31cf1
denis <denispolyarush1405@gmail.com>
2024-03-14 14:47:18 +0300

    commit after consultation

--- src/index.js
+++ src/index.js
@@ -1,58 +1,48 @@
 'use strict';
 
-class Person {
-  regExpEmail = /^\w+\.?\w+@[a-z]{3,8}\.[a-z]{2,5}$/gi;
+const regExpEmail = /^\w+\.?\w+@[a-z]{3,8}\.[a-z]{2,5}$/gi;
 
-  constructor(fName, lName, dName, email) {
-    this.fName = fName;
-    if (!fName) throw new Error("Don't entered fName");
-    this.lName = lName;
-    if (!lName) throw new Error("Don't entered lName");
-    this.dName = dName;
-    if (!dName) throw new Error("Don't entered dName");
-    this.email = email;
+class Person {
+  constructor(...arg) {
+    arg.fName = fName;
+    arg.lName = lName;
+    arg.dName = dName;
+    arg.email = email;
     if (!this.regExpEmail.test(this.email))
       throw new Error('Email entered incorrectly');
   }
 }
 
-const btnOk = document.getElementById('ok');
+const userInfo = document.querySelectorAll('input');
 
-function changeInput(e) {
-  const userInfo = document.querySelectorAll('#fname, #lname, #dname, #email');
-  try {
-    const arrayUserInfo = Array.from(userInfo).map((el) => el.value);
-    const [fName, lName, dName, email] = arrayUserInfo;
-    const user = new Person(fName, lName, dName, email);
-    localStorage.setItem(`${lName}`, JSON.stringify(user));
-  } catch (error) {
-    e.preventDefault();
-    console.log(error.message);
-    if (error.message === "Don't entered fName") {
-      fname.style.outline = 'solid 5px red';
-    } else {
-      fname.style.outline = '';
-    }
-    if (error.message === "Don't entered lName") {
-      lname.style.outline = 'solid 5px red';
-    } else {
-      lname.style.outline = '';
-    }
-    if (error.message === "Don't entered dName") {
-      dname.style.outline = 'solid 5px red';
-    } else {
-      dname.style.outline = '';
-    }
-    if (error.message === 'Email entered incorrectly') {
-      email.style.outline = 'solid 5px red';
-      const errorText = document.createElement('span');
-      errorText.setAttribute('id', 'errorSpan')
-      errorText.textContent = 'Entered wrong email';
-      emailDiv.appendChild(errorText);
-    }
-  }
+function collectProps(e) {
+  console.log(userInfo);
+  const arrayUserInfo = Array.from(userInfo).map((el) => el.value);
+  console.log(arrayUserInfo);
+  const user = new Person(
+    arrayUserInfo.fName,
+    arrayUserInfo.lName,
+    arrayUserInfo.dName,
+    arrayUserInfo.email
+  );
+  console.log(user);
 }
 
-btnOk.addEventListener('click', changeInput, {
-  capture: true,
-});
+// const btnOk = document.getElementById('ok');
+
+// function changeInput() {
+//   e.preventDefault();
+//   localStorage.setItem(`${lName}`, JSON.stringify(user));
+// }
+
+userInfo.forEach((el) => { 
+  el.value.addEventListener('change', collectProps)
+})
+ 
+// if (error.message === 'Email entered incorrectly') {
+//   email.style.outline = 'solid 5px red';
+//   const errorText = document.createElement('span');
+//   errorText.setAttribute('id', 'errorSpan');
+//   errorText.textContent = 'Entered wrong email';
+//   emailDiv.appendChild(errorText);
+// }
